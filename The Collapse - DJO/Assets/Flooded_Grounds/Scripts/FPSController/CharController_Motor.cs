7a21f41daa1700aea4e6494fefd31d9d9d7bfc28
Felipe <felipevendrami@hotmail.com>
2024-11-20 16:57:03 -0300

    Movimentacao do personagem

--- The Collapse - DJO/Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
+++ The Collapse - DJO/Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class CharController_Motor : MonoBehaviour {
 
 	public float speed = 10.0f;
-	public float sensitivity = 30.0f;
+	public float sensitivity = 70.0f;
 	public float WaterHeight = 15.5f;
 	CharacterController character;
 	public GameObject cam;
 	float moveFB, moveLR;
 	float rotX, rotY;
 	public bool webGLRightClickRotation = true;
-	float gravity = -9.8f;
+	//public float gravity = -9.8f;
+	public float gravity = -40f;
+
+    // Movimento Pulo
+	public float alturaPulo = 10f;
+	public Transform checaChao;
+    public float raioEsfera = 0.4f;
+    public LayerMask chaoMask;
+    public bool estaNoChao;
+
+	Vector3 velocidadeCai;
+
+	// Movimento Agachar
+    private Transform cameraTransform;
+    private bool estahAbaixado = false;
+    private bool levantarBloqueado;
+    public float alturaLevantado, alturaAbaixado, posicaoCameraEmPe, posicaoCameraAbaixado;
 
 
 	void Start(){
 		//LockCursor ();
 		character = GetComponent<CharacterController> ();
+		cameraTransform = cam.transform;
 		if (Application.isEditor) {
 			webGLRightClickRotation = false;
 			sensitivity = sensitivity * 1.5f;
@@ -42,15 +59,9 @@ public class CharController_Motor : MonoBehaviour {
 		rotX = Input.GetAxis ("Mouse X") * sensitivity;
 		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
 
-		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
-		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
-
 		CheckForWaterHeight ();
 
-
 		Vector3 movement = new Vector3 (moveFB, gravity, moveLR);
-
-
 
 		if (webGLRightClickRotation) {
 			if (Input.GetKey (KeyCode.Mouse0)) {
@@ -60,17 +71,67 @@ public class CharController_Motor : MonoBehaviour {
 			CameraRotation (cam, rotX, rotY);
 		}
 
+		// Para pulo
+		// Cria uma esfera de raioEsfera na posicao do checaChao, batendo com a mascara do chao
+        // se estah em contato com o chaoMask, entao retorna true
+        estaNoChao = Physics.CheckSphere(checaChao.position, raioEsfera, chaoMask);
+
+        if (!levantarBloqueado && estaNoChao && Input.GetButtonDown("Jump"))
+        {
+            velocidadeCai.y = Mathf.Sqrt(alturaPulo * -2f * gravity);
+        }
+        
+        if (!estaNoChao)
+        {
+            velocidadeCai.y += gravity * Time.deltaTime;
+        }
+
+		ChecarBloqueioAbaixado();
+
+		// Para agachar e lavantar
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            AgacharLevantar();
+        }
+
 		movement = transform.rotation * movement;
 		character.Move (movement * Time.deltaTime);
 	}
-
 
 	void CameraRotation(GameObject cam, float rotX, float rotY){		
 		transform.Rotate (0, rotX * Time.deltaTime, 0);
 		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
 	}
 
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(checaChao.position, raioEsfera);
+    }
 
+    private void AgacharLevantar()
+    {
+        if (levantarBloqueado || estaNoChao == false)
+        {
+            return;
+        }
+        
+        estahAbaixado = !estahAbaixado;
+        if (estahAbaixado)
+        {
+            character.height = alturaAbaixado;
+            cameraTransform.localPosition = new Vector3(0, posicaoCameraAbaixado, 0);
+        } else
+        {
+            character.height = alturaLevantado;
+            cameraTransform.localPosition = new Vector3(0, posicaoCameraEmPe, 0);
+        }
+    }
 
-
+	private void ChecarBloqueioAbaixado()
+    {
+        Debug.DrawRay(cameraTransform.position, Vector3.up * 1.1f, Color.red);
+        RaycastHit hit;
+        levantarBloqueado = Physics.Raycast(cameraTransform.position, Vector3.up, out hit, 1.1f);
+    }
 }
